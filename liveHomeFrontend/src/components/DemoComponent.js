264ab91930b82aef9efc74870a05b48cb2d92a4f
Fabian Toro <helber.toro@gmail.com>
2020-05-10 16:32:44 -0500

    Modal component

--- liveHomeFrontend/src/components/DemoComponent.js
+++ liveHomeFrontend/src/components/DemoComponent.js
@@ -1,6 +1,7 @@
 import styles from '../styles/components/DemoComponent.module.sass';
 import ButtonIcon from './ButtonIcon';
 import RadioButton from './RadioButton';
+import Modal from './Modal';
 
 function DemoComponent () {
   return (
@@ -19,6 +20,10 @@ function DemoComponent () {
       <ButtonIcon icon='check' size='24px' />
       <h4>Radio Button</h4>
       <RadioButton options={['yes', 'no']} name='test' />
+      <h4>Modal</h4>
+      <Modal buttonText='Abril modal'>
+        <p>Este es el conetido del modal</p>
+      </Modal>
     </>
   );
 }
--- liveHomeFrontend/src/components/RadioButton.js
+++ liveHomeFrontend/src/components/RadioButton.js
@@ -4,9 +4,9 @@ import styles from '../styles/components/RadioButton.module.sass';
 function RadioButton ({options, name}) {
   return (
     <>
-    {options.map((item) => {
+    {options.map((item, key) => {
       return (
-        <div className={styles.radio}>
+        <div key={key} className={styles.radio}>
           <input id={item} type="radio" name={name} value={item}/> 
           <label htmlFor={item}>{item}</label>
         </div>
