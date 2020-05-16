129b2c3e4584aca4c83ffd27ad05c7c2851d6502
Jose Luis Campos Bautista <bautistaj20@hotmail.com>
2020-05-16 18:00:09 -0500

    Add validation of end-point file

--- liveHomeBackend/src/routes/file.js
+++ liveHomeBackend/src/routes/file.js
@@ -1,6 +1,8 @@
 'use strict'
 const express = require('express')
 const FileService = require('./../services/file')
+const validationHandler = require('./../utils/middleware/validationHandler')
+const { fileIdSchema, fileCreateSchema, fileUpdateSchema } = require('./../utils/schemas/file')
 
 function fileApi (app) {
   const router = express()
@@ -21,51 +23,58 @@ function fileApi (app) {
     }
   })
 
-  router.get('/:id', async function (req, res, next) {
-    try {
-      const { id } = req.params
+  router.get('/:id',
+    validationHandler({ id: fileIdSchema }, 'params'),
+    async function (req, res, next) {
+      try {
+        const { id } = req.params
 
-      const file = await fileService.getById(id)
+        const file = await fileService.getById(id)
 
-      res.status(200).json({
-        data: file || {},
-        message: 'File retrieved'
-      })
-    } catch (error) {
-      next(error)
-    }
-  })
+        res.status(200).json({
+          data: file || {},
+          message: 'File retrieved'
+        })
+      } catch (error) {
+        next(error)
+      }
+    })
 
-  router.put('/:id', async function (req, res, next) {
-    try {
-      const { id } = req.params
-      const { body: file } = req
+  router.put('/:id',
+    validationHandler({ id: fileIdSchema }, 'params'),
+    validationHandler(fileUpdateSchema),
+    async function (req, res, next) {
+      try {
+        const { id } = req.params
+        const { body: file } = req
 
-      const fileUpdated = await fileService.update(id, file)
+        const fileUpdated = await fileService.update(id, file)
 
-      res.status(200).json({
-        data: fileUpdated,
-        message: 'File updated'
-      })
-    } catch (error) {
-      next(error)
-    }
-  })
+        res.status(200).json({
+          data: fileUpdated,
+          message: 'File updated'
+        })
+      } catch (error) {
+        next(error)
+      }
+    })
 
-  router.post('/', async function (req, res, next) {
-    try {
-      const { body: file } = req
+  router.post('/',
+    validationHandler(fileCreateSchema),
+    async function (req, res, next) {
+      try {
+        const { body: file } = req
 
-      const fileCreated = await fileService.create(file)
+        const fileCreated = await fileService.create(file)
 
-      res.status(201).json({
-        data: fileCreated,
-        message: 'File created'
-      })
-    } catch (error) {
-      next(error)
-    }
-  })
+        res.status(201).json({
+          data: fileCreated,
+          message: 'File created'
+        })
+      } catch (error) {
+        next(error)
+      }
+    })
 }
 
 module.exports = fileApi
