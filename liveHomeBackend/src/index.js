437d0b11b0a286ce5d85bc36ae3cbb9576889081
sjimen13 <sjimen13@gmail.com>
2020-05-21 14:42:12 -0500

    no

--- liveHomeBackend/src/index.js
+++ liveHomeBackend/src/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const config = require('./../config')
 
-
 // Middleware
 const notFoundHandler = require('./utils/middleware/notFoundHandler')
 const { errorHandler, logError, wrapError } = require('./utils/middleware/errorHandler')
