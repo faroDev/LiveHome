4de89ca8bd30c7d5acfed86c536bbd7e0730c3f9
Leonardo Beltran <leonardo.casallas.beltran@gmail.com>
2020-05-24 22:26:29 -0500

    [feature] test add api register page

--- liveHomeFrontend/pages/register.js
+++ liveHomeFrontend/pages/register.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Router from 'next/router';
 import useInputValue from '../src/hooks/useInputValue';
+import API from '../src/utils/api';
 
 import Layout from '../src/components/Layout';
 import Form from '../src/components/Form';
 import FormField from '../src/components/FormField';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
+import Loading from '../src/components/Loading';
+import Error from '../src/components/Error';
+import AlertField from '../src/components/AlertField';
 
 import styles from '../src/styles/pages/register.module.sass';
 
@@ -13,30 +18,67 @@ const Register = () => {
   const email = useInputValue('');
   const password = useInputValue('');
   const repeatPassword = useInputValue('');
+  const [loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(`Email: ${email.value}, Password: ${password.value}, RePassword: ${repeatPassword.value}`);
+    setLoading(true);
+    
+    const data = { email: email.value, password: password.value, userName: email.value };
+
+    try {
+      const response = await API.signUp(data);
+      console.log('[respuesta register ok]',response);
+      setLoading(false);
+      if(response.data.id > 0 ){
+        Router.push('/login');
+      }
+    } catch (error) {
+      console.error('[error]', error);
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  const passwordValidate = () => {
+    if (password.value === repeatPassword.value) {
+      return false
+    }
+    return true;
   };
 
   return (
     <Layout>
       <div className={styles.register__container}>
         <h3>Register</h3>
-        <Form onSubmit={handleSubmit}>
-          <FormField>
-            <Input type='email' label='Email' {...email} required name='email' />
-          </FormField>
-          <FormField>
-            <Input type='password' label='Password' {...password} required name='password' />
-          </FormField>
-          <FormField>
-            <Input type='password' label='Repeat password' {...repeatPassword} required name='repeatpassword' />
-          </FormField>
-          <FormField>
-            <Button value='Sign up' buttonType='submit' buttonClass='greenButton' />
-          </FormField>
-        </Form>
+        {
+          loading && <Loading />
+        }
+        {
+          error && <Error error={error} />
+        }
+        {
+          !loading && !error && 
+          <Form onSubmit={handleSubmit}>
+            <FormField>
+              <Input type='email' label='Email' {...email} required name='email' />
+            </FormField>
+            <FormField>
+              <Input type='password' label='Password' {...password} required name='password' />
+            </FormField>
+            <FormField>
+              <Input type='password' label='Repeat password' {...repeatPassword} required name='repeatpassword' />
+              {
+                passwordValidate() &&
+                <AlertField message={`Fields password and repeat password isn't equals, please check values`} />
+              }
+            </FormField>
+            <FormField>
+              <Button value='Sign up' buttonType='submit' buttonClass='greenButton' disabled={passwordValidate()} />
+            </FormField>
+          </Form>
+        }
       </div>
     </Layout>
   );
