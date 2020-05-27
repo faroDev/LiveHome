dac500c0cb56f18106ad68a7761c981ecbd31c35
Jose Roberto Diaz Rodriguez <diazrodriguez.jr@gmail.com>
2020-05-26 19:25:35 -0500

    Connect backend to heroku postgres

--- liveHomeBackend/store/lib/db.js
+++ liveHomeBackend/store/lib/db.js
@@ -1,21 +1,25 @@
-'use strict'
+'use strict';
 
-const Sequelize = require('sequelize')
-let sequelize = null
+const Sequelize = require('sequelize');
+let sequelize = null;
 
 module.exports = function setupDataBase (config) {
-  const connectionString = 'postgres://jiaeqcafqfayou:1d4431cbc00ec7ae74cb68c3ba3271813a35b60f11ac9cb65737df01607298e3@ec2-174-129-255-69.compute-1.amazonaws.com:5432/dappc77f3sd74a'
   if (!sequelize) {
-    console.log('Init sequelize')
-    sequelize = new Sequelize(connectionString, {
+    console.log('Init sequelize', config);
+
+    const options = {
       dialect: 'postgres',
-      protocol: 'postgres',
-      port: 5432,
-      host: 'ec2-174-129-255-69.compute-1.amazonaws.com',
+      ssl: true,
       dialectOptions: {
-        ssl: true
-      }
-    })
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      returning: true,
+      setup: true
+    };
+    sequelize = new Sequelize({ ...config, ...options });
   }
-  return sequelize
-}
+  return sequelize;
+};
