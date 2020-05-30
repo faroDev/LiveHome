c808b7c595100ded28eead252f88af1d481ab2cf
Jose Luis Campos Bautista <bautistaj20@hotmail.com>
2020-05-30 16:35:07 -0500

    Implement test service property type.

--- liveHomeBackend/src/mocks/dataBaseMock.js
+++ liveHomeBackend/src/mocks/dataBaseMock.js
@@ -2,10 +2,12 @@
 const status = require('./dataBaseMock/statusDatabaseMock')
 const zones = require('./dataBaseMock/statusDatabaseMock')
 const modalityType = require('./dataBaseMock/statusDatabaseMock')
+const propertyType = require('./dataBaseMock/statusDatabaseMock')
 module.exports = async function (config) {
   return {
     status,
     zones,
-    modalityType
+    modalityType,
+    propertyType
   }
 }
--- liveHomeBackend/src/test/service/service.propertyType.test.js
+++ liveHomeBackend/src/test/service/service.propertyType.test.js
@@ -6,35 +6,37 @@ const proxyquire = require('proxyquire')
 const dataBaseMock = require('./../../mocks/dataBaseMock')
 const { create, findAll, update, findById } = require('./../../mocks/dataBaseMock/statusDatabaseMock')
 
-describe('Services - status', function () {
-  const StatusService = proxyquire('./../../services/status.js', {
+describe('Services - property type', function () {
+  const PropertyTypeService = proxyquire('./../../services/propertyType.js', {
     './../../store/index': dataBaseMock
   })
 
-  const statusService = new StatusService()
+  const propertyTypeService = new PropertyTypeService()
 
   describe('When get method is called', async function () {
     it('* should call the findAll method', async function () {
-      await statusService.get()
+      await propertyTypeService.get()
       assert.strictEqual(findAll.called, true)
     })
+  })
 
+  describe('When get getById is called', async function () {
     it('* should call the findById method', async function () {
-      await statusService.getById(1)
+      await propertyTypeService.getById(1)
       assert.strictEqual(findById.called, true)
     })
   })
 
   describe('When post method is called', async function () {
     it('* should call the create method', async function () {
-      await statusService.create({})
+      await propertyTypeService.create({})
       assert.strictEqual(create.called, true)
     })
   })
 
   describe('When update method is called', async function () {
     it('* should call the update method', async function () {
-      await statusService.update(1, {})
+      await propertyTypeService.update(1, {})
       assert.strictEqual(update.called, true)
     })
   })
